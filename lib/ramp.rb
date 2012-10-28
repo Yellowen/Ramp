43d76815eaf37447a9c33c27d909454fd149a49a
Sameer Rahmani <lxsameer@gnu.org>
2012-10-28 20:27:44 +0330

    basic interface done

--- lib/ramp.rb
+++ lib/ramp.rb
@@ -17,18 +17,98 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 # -----------------------------------------------------------------------------
 require 'socket'
+require 'stringio'
+
 require "ramp/version"
 
+
 module Ramp
-  class AMP
-    def initialize (host, port, secure=false, ssl_key=nil, ssl_cert=nil)
-      @socket = TCPSocket.new host, port
+  # Ramp module
+  
+  class AmpPacket
+    # This class is responsble for packing and unpacking the AMP protocol pack
+
+    def initialize(kwargs={})
+      @_kwargs = kwargs
+      @buffer = []
+      self.generate_packet()
+    end
+
+    def to_s
+      # Build a AMP packet data from kwargs hash for more information about
+      # amp protocol structure take a look at:
+      # http://www.amp-protocol.net
+
+      @buffer.pack("c*")
 
     end
 
-    def call_remote(packet, *kwargs)
-      @socket.puts(packet)
+    def to_a
+      @buffer
+    end
+
+    def split_bytes (hex)
+      hex.each_char.each_slice(2).map {|x| x.join}
+    end
+
+    def generate_packet()
+      @_kwargs.each do |key, value|
+
+        if key.length > 255
+          raise AmpAskPacket::KeyLenError, "AMP keys should have 255 byte max kength"
+        end
+        
+        [0, key.length].each {|x| @buffer << x}
+        key.to_s.bytes.to_a.each {|x| @buffer << x}
+
+        value_lenght = self.split_bytes "%04x" % value.to_s.length.to_s(16)
+        @buffer << value_lenght[0].to_i
+        @buffer << value_lenght[1].to_i
+        
+        value.to_s.bytes.to_a.each {|x| @buffer << x}
+        
+      end
+
+      [0x00, 0x00].each {|x| @buffer << x}
+
+    end
+  end
+
+
+  class AmpClient
+    # AmpClient class is responsble for establishing a connection to a AMPserver
+
+    # A Class variable (static attribute) to hold the asks sequences
+    @@ask_seqs = []
+
+    def initialize (host, port, secure=false, ssl_key=nil, ssl_cert=nil)
+      begin
+        @socket = TCPSocket.new host, port
+      rescue Errno::ECONNREFUSED
+        abort("Connection Refused")
+      end
+
+    end
+
+    def call_remote(func, kwargs)
+      ask = rand(999999) until not @@ask_seqs.include? ask
+      @@ask_seqs << ask
+
+      kwargs = Hash[kwargs.map{|k, v|[k.to_sym, v]}]
+      if kwargs.include? :_ask or kwargs.include? :_command
+        raise ArgumentError, "':_ask' and ':_command' should not be in kwargs"
+      end
+
+      command_struct = {
+        :_ask => ask,
+        :_command => func,
+      }
+
+      command_struct.merge!(kwargs)
+
+      @socket.puts(AmpPacket.new(command_struct).to_s)
     end
     
   end
+
 end
